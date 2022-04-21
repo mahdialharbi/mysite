d967a10321c5440ed25bba33dce83db1eeb70eac
Mahdi Alharbi <mahdialharbi@Mahdis-MacBook-Pro.local>
2022-04-21 04:51:05 -0700

    adding the frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import Home from './Home/Home';
-import './App.css';
+import React from "react";
+import Home from "./Home/Home";
+import Leaders from "./Players/Leaders";
+import "./App.css";
 
 const App = () => (
-  <div className="Home">
-    <Home />
+  <div className="Leaders">
+    <Leaders />
   </div>
 );
 
